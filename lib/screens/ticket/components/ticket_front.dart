9dff77a0708dca49a9e0d0ce190aace34950c98e
Martin Osama <martinosama2015@gmail.com>
2020-11-04 17:39:25 +0200

    Modified TicketFront view and Added TicketBack view with 3D rotation with slider

--- lib/screens/ticket/components/ticket_front.dart
+++ lib/screens/ticket/components/ticket_front.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:movie_app/constants.dart';
@@ -20,116 +22,130 @@ class _TicketFrontState extends State<TicketFront> {
         child: Container(
           height: 250,
           decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(15),
-              gradient: RadialGradient(colors: [
-                const Color(0xFF1b1a16),
-                const Color(0xFF1b1a16),
-                Colors.black
-              ], center: Alignment(0.5, 0.1))),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
+              borderRadius: BorderRadius.circular(15), color: Colors.black),
+          child: Stack(
             children: [
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  SvgPicture.asset(
-                    "assets/images/cinema.svg",
-                    color: Colors.white,
-                    height: 150,
-                    width: 150,
-                  ),
-                  Column(
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
                     children: [
-                      Padding(
-                        padding: const EdgeInsets.symmetric(vertical: 5),
-                        child: Text(
-                          "IMAX",
-                          style:
-                              TextStyle(color: kSecondaryColor, fontSize: 20),
-                        ),
-                      ),
-                      Text(
-                        "CINEMA",
-                        style: TextStyle(
-                            fontSize: 50,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.white),
-                      ),
-                      Text(
-                        "TICKET",
-                        style: TextStyle(
-                            fontSize: 50,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.white),
+                      Column(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.symmetric(vertical: 5),
+                            child: Text(
+                              "IMAX",
+                              style: TextStyle(
+                                  color: kSecondaryColor, fontSize: 20),
+                            ),
+                          ),
+                          Padding(
+                            padding:
+                                const EdgeInsets.symmetric(horizontal: 8.0),
+                            child: Text(
+                              "CINEMA TICKET  ",
+                              style: TextStyle(
+                                  fontSize: 40,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.white),
+                            ),
+                          ),
+                          Text(
+                            "MOVIE NAME",
+                            style: TextStyle(
+                                fontSize: 40,
+                                fontWeight: FontWeight.bold,
+                                color: kSecondaryColor),
+                          )
+                        ],
                       )
                     ],
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(kDefaultPadding),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Column(
+                          children: [
+                            Text(
+                              "THEATRE",
+                              style: TextStyle(color: kSecondaryColor),
+                            ),
+                            Text(
+                              "02",
+                              style:
+                                  TextStyle(fontSize: 35, color: Colors.white),
+                            )
+                          ],
+                        ),
+                        Column(
+                          children: [
+                            Text(
+                              "ROW",
+                              style: TextStyle(color: kSecondaryColor),
+                            ),
+                            Text(
+                              "08",
+                              style:
+                                  TextStyle(fontSize: 35, color: Colors.white),
+                            )
+                          ],
+                        ),
+                        Column(
+                          children: [
+                            Text(
+                              "SEAT",
+                              style: TextStyle(color: kSecondaryColor),
+                            ),
+                            Text(
+                              "67",
+                              style:
+                                  TextStyle(fontSize: 35, color: Colors.white),
+                            )
+                          ],
+                        ),
+                        Column(
+                          children: [
+                            Text(
+                              "BARCODE: 1234ABCD",
+                              style: TextStyle(color: kSecondaryColor),
+                            ),
+                            BarCodeImage(
+                              params: Code39BarCodeParams(
+                                "1234ABCD",
+                                lineWidth:
+                                    1.0, // width for a single black/white bar (default: 2.0)
+                                barHeight:
+                                    40.0, // height for the entire widget (default: 100.0)
+                                // Render with text label or not (default: false)
+                              ),
+                              onError: (error) {
+                                // Error handler
+                                print('error = $error');
+                              },
+                            ),
+                          ],
+                        )
+                      ],
+                    ),
                   )
                 ],
               ),
-              Padding(
-                padding: const EdgeInsets.all(kDefaultPadding),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Column(
-                      children: [
-                        Text(
-                          "THEATRE",
-                          style: TextStyle(color: kSecondaryColor),
-                        ),
-                        Text(
-                          "02",
-                          style: TextStyle(fontSize: 35, color: Colors.white),
-                        )
-                      ],
-                    ),
-                    Column(
-                      children: [
-                        Text(
-                          "ROW",
-                          style: TextStyle(color: kSecondaryColor),
-                        ),
-                        Text(
-                          "08",
-                          style: TextStyle(fontSize: 35, color: Colors.white),
-                        )
-                      ],
-                    ),
-                    Column(
-                      children: [
-                        Text(
-                          "SEAT",
-                          style: TextStyle(color: kSecondaryColor),
-                        ),
-                        Text(
-                          "67",
-                          style: TextStyle(fontSize: 35, color: Colors.white),
-                        )
-                      ],
-                    ),
-                    Column(
-                      children: [
-                        Text(
-                          "BARCODE: 1234ABCD",
-                          style: TextStyle(color: kSecondaryColor),
-                        ),
-                        BarCodeImage(
-                          params: Code39BarCodeParams(
-                            "1234ABCD",
-                            lineWidth:
-                                1.0, // width for a single black/white bar (default: 2.0)
-                            barHeight:
-                                40.0, // height for the entire widget (default: 100.0)
-                            // Render with text label or not (default: false)
-                          ),
-                          onError: (error) {
-                            // Error handler
-                            print('error = $error');
-                          },
-                        ),
-                      ],
-                    )
-                  ],
+              Positioned(
+                bottom: 0,
+                left: 0,
+                child: Transform.rotate(
+                  angle: -15 / 180 * pi,
+                  child: SvgPicture.asset(
+                    "assets/images/cinema.svg",
+                    color: Colors.white.withOpacity(0.2),
+                    width: 180,
+                    height: 180,
+                  ),
                 ),
               )
             ],
--- lib/screens/ticket/ticket_screen.dart
+++ lib/screens/ticket/ticket_screen.dart
@@ -1,9 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:movie_app/screens/ticket/components/ticket_back.dart';
 import 'package:movie_app/screens/ticket/components/ticket_front.dart';
+import 'dart:math' as math;
 
-class TicketScreen extends StatelessWidget {
+class TicketScreen extends StatefulWidget {
+  @override
+  _TicketScreenState createState() => _TicketScreenState();
+}
+
+class _TicketScreenState extends State<TicketScreen> {
+  double rotationDegree = 0;
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: TicketFront());
+    return Scaffold(
+        body: Stack(
+      children: [
+        Center(
+          child: Container(
+            height: 400,
+            child: Column(
+              children: [
+                Transform(
+                    transform: Matrix4.identity()
+                      ..setEntry(3, 2, 0.001)
+                      ..rotateY(math.pi * rotationDegree),
+                    origin: Offset(MediaQuery.of(context).size.width / 2, 0),
+                    child: rotationDegree < 0.5 ? TicketFront() : TicketBack()),
+                Slider(
+                    value: rotationDegree,
+                    onChanged: (double value) {
+                      setState(() {
+                        rotationDegree = value;
+                      });
+                    })
+              ],
+            ),
+          ),
+        ),
+        SafeArea(child: BackButton()),
+      ],
+    ));
   }
 }
